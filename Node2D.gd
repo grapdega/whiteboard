a33a6206cf46c52ebde17947fb490424202bf7ef
sulincix <parduscix@yandex.ru>
2022-10-31 20:48:50 +0000

    line quality improvement

--- Node2D.gd
+++ Node2D.gd
@@ -7,16 +7,22 @@ onready var _lines := $Lines
 
 var _pressed := false
 var _current_line: Line2D
-
+var lastpos = null
 func _input(event:InputEvent) -> void:
 	if event is InputEventMouseButton:
 		_pressed = event.pressed
 		
 		if _pressed:
 			_current_line = Line2D.new()
+			lastpos = null
 			_current_line.default_color = Color.white
 			_current_line.width = 3
 			_lines.add_child(_current_line)
 			_current_line.antialiased = true
 	if event is InputEventMouseMotion && _pressed:
-		_current_line.add_point(event.position)
+		var epos = event.position
+		if lastpos != null:
+			epos.x = (lastpos.x + epos.x) /2
+			epos.y = (lastpos.y + epos.y) /2
+		lastpos = event.position
+		_current_line.add_point(epos)
