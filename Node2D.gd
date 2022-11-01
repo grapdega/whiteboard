37a795057d34fcdc12ac6ac18c6978035119ea0d
aliriza <aliriza.keskin@pardus.org.tr>
2022-11-01 09:50:45 +0000

    fix touch & mouse conflict

--- Node2D.gd
+++ Node2D.gd
@@ -17,21 +17,21 @@ func _input(event:InputEvent) -> void:
 			_curve.append(state[event.index])
 			state.erase(event.index)
 
-	if event is InputEventMouseButton:
+	elif event is InputEventMouseButton:
 		_pressed = event.pressed
 		if _pressed:
 			_curve.append(Curve2D.new())
 			lastpos = null
 	if _pressed:
-		if event is InputEventMouseMotion:
+		if event is InputEventScreenDrag:
+			state[event.index].add_point(event.position)
+		elif event is InputEventMouseMotion:
 			var epos = event.position
 			if lastpos != null:
 				epos.x = (lastpos.x + epos.x) /2
 				epos.y = (lastpos.y + epos.y) /2
 			lastpos = event.position
 			_curve[-1].add_point(epos)
-		if event is InputEventScreenDrag:
-			state[event.index].add_point(event.position)
 	update()
 
 func _draw():
