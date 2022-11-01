0c9af9a1c5f5649ebe03cda6dec192ddcc37b187
aliriza <aliriza.keskin@pardus.org.tr>
2022-11-01 06:40:17 +0000

    curve2d

--- Node2D.gd
+++ Node2D.gd
@@ -1,28 +1,29 @@
 extends Node2D
 
 func _ready():
+	_curve = []
 	get_tree().get_root().set_transparent_background(true)
 
-onready var _lines := $Lines
-
 var _pressed := false
-var _current_line: Line2D
+var _curve = null
 var lastpos = null
+
 func _input(event:InputEvent) -> void:
 	if event is InputEventMouseButton:
 		_pressed = event.pressed
-		
 		if _pressed:
-			_current_line = Line2D.new()
+			_curve.append(Curve2D.new())
 			lastpos = null
-			_current_line.default_color = Color.white
-			_current_line.width = 3
-			_lines.add_child(_current_line)
-			_current_line.antialiased = true
 	if event is InputEventMouseMotion && _pressed:
 		var epos = event.position
 		if lastpos != null:
 			epos.x = (lastpos.x + epos.x) /2
 			epos.y = (lastpos.y + epos.y) /2
 		lastpos = event.position
-		_current_line.add_point(epos)
+		_curve[-1].add_point(epos)
+	update()
+
+func _draw():
+	if _curve != null:
+		for _c in _curve:
+			draw_polyline(_c.get_baked_points(),Color.white,2.0,true)
